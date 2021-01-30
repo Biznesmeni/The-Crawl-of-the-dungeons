7229f153efd3d79e13d3df5ee2eeae91d820dd83
Dāvis <davis.ositis@gmail.com>
2021-01-30 17:07:30 +0200

    Create ClassDruid.py

---
+++ ClassFeatures/ClassDruid.py
@@ -0,0 +1,15 @@
+from EquipmentVariables import *
+from PlayerVariables import *
+from ClassVariables import*
+
+if ClassDruid == True:
+    PlayerManaMax = 20
+    PlayerManaCurrent = 20
+    PlayerHealthMax = 8
+    PlayerHealthCurrent = 8
+    PlayerDamage = 2
+    Staff = True
+print (playerManaMax)
+    
+    
+    
