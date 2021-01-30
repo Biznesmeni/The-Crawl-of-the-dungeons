2a74261996ccd50156bf8281f9f27777ae374808
Dāvis <davis.ositis@gmail.com>
2021-01-30 17:40:10 +0200

    Update ClassDruid.py

--- ClassFeatures/ClassDruid.py
+++ ClassFeatures/ClassDruid.py
@@ -5,13 +5,13 @@ from ClassVariables import*
 if ClassDruid == True:
     PlayerManaMax = 20
     PlayerManaCurrent = 20
-    PlayerHealthMax = 8
-    PlayerHealthCurrent = 8
+    PlayerHealthMax = 14
+    PlayerHealthCurrent = 14
     PlayerDamage = 2
     Staff = True
     PlayerDodgechance = 5
     PlayerHitChance = 70
-print (playerManaMax)
+
     
     
     
