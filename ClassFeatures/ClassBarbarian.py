790f50e6ea31fef3e60483733ac7a8d2b90c00f2
S0ULR3PER <52076039+S0ULR3PER@users.noreply.github.com>
2021-01-30 17:50:24 +0200

    Create ClassBarbarian.py

---
+++ ClassFeatures/ClassBarbarian.py
@@ -0,0 +1,14 @@
+from ClassVariables import *
+from EquipmentVariables import *
+from PlayerVariables import *
+
+if ClassBarbarian==True:
+    PlayerhealthMax = 25
+    PlayerHealthCurrent = 25
+    PlayerManaMax = 0
+    PlayerManaCurrent = 0
+    PlayerDamage = 5
+    PlayerDodgeChance = 0
+    PlayerHitChance = 65
+    Axe=True
+    ArmorHeavy=True
