635b2b9c93b6721baabc7071fc1f6f6e16da01d4
Dāvis <davis.ositis@gmail.com>
2021-01-30 17:59:06 +0200

    Update ItemsWhenEquipped.py

--- ItemEffects/ItemsWhenEquipped.py
+++ ItemEffects/ItemsWhenEquipped.py
@@ -4,17 +4,18 @@ from PlayerVariables import *
 
 
 if ArmorLight == True:
-    PlayerDefense=1
+    PlayerDefense=1 + DefenseModifier 
     
 if ArmorHeavy == True: 
-    PlayerDefense=2  
+    PlayerDefense=2 + DefenseModifier 
         
 if Sword == True:
-    PlayerDamage=4
+    PlayerDamage=4 + AttackModifier
+ 
 
 if Staff == True:
-    PlayerManaMax=10
+    PlayerManaMax=10 + ManaModifier
     
 if Axe == True:
-    PlayerDamage=5
+    PlayerDamage=5 + AttackModifier
  
