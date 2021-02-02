898b705a9fa06d93c3a225849a0cb1210def35f3
Dāvis <davis.ositis@gmail.com>
2021-02-02 18:44:45 +0200

    Atklāju jaunu veidu, kā kategorizēt.

--- ClassFeatures/ClassBarbarian.py
+++ ClassFeatures/ClassBarbarian.py
@@ -1,15 +1,16 @@
 from ClassVariables import *
 from EquipmentVariables import *
 from PlayerVariables import *
+class ClassBarbarian:
+   if ClassBarbarian==True:
+        PlayerhealthMax = 25
+        PlayerHealthCurrent = 25
+        PlayerManaMax = 0
+        PlayerManaCurrent = 0
+        PlayerDamage = 5
+        PlayerDodgeChance = 5
+        PlayerHitChance = 65
+        Axe=True
+        ArmorHeavy=True
 
-if ClassBarbarian==True:
-    PlayerhealthMax = 25
-    PlayerHealthCurrent = 25
-    PlayerManaMax = 0
-    PlayerManaCurrent = 0
-    PlayerDamage = 5
-    PlayerDodgeChance = 5
-    PlayerHitChance = 65
-    Axe=True
-    ArmorHeavy=True
 
