d84650b128a398bd6f920d5601b9be4a6ca01ffa
Dāvis <davis.ositis@gmail.com>
2021-02-04 21:02:17 +0200

    Pabeidzu screen

--- MainMenu/MainMenu.py
+++ MainMenu/MainMenu.py
@@ -1,8 +1,17 @@
 import pygame #importē pygame lai var izmantot funkcijas
 
+BackgroundImg = pygame.image.load('Background_Menu(PlaceHolder).png')#atrod attēlu folderī
+BackgroundX = 0 #kur atradīsies attēls x asī
+BackgroundY = 0 #kur atradīsies attēls y asī
+
+def background():#izveido jaunu funkciju
+    screen.blit(BackgroundImg, (BackgroundX,BackgroundY)) #izprintē pāsu attēlu
+    
+ 
+  
 pygame.init()#vajadzīgs, lai inicializētu funkcijas
 
-screen = pygame.display.set_mode((480,270))#izveido ekrānu
+screen = pygame.display.set_mode((960,540))#izveido ekrānu (width,heigth)
 
 pygame.display.set_caption('Piemērs')#Pārmaina ekrāna spēles nosaukumu ekrāna kreisajā augšā
 icon = pygame.image.load('Sprite-0002.png')#Dabū attēlu ikonai, jābūt 32x32 pikseļi
@@ -14,7 +23,17 @@ while running:
     for event in pygame.event.get():#Pārbauda, vai kaut kas notiek
         if event.type == pygame.QUIT:#uztver klikšķi uz x pogas
             running = False
-    screen.fill((255,165,0))#aizkrāso ekrānu oranžu RGB - (Red, Green, Blue)
+    # screen.fill((255,165,0))#aizkrāso ekrānu oranžu RGB - (Red, Green, Blue)
+    
+    background()#iepriek veidotā funkcija
     pygame.display.update()#Updato ekrānu, lai izdēstu bijusos framus.
+    
+    
+
+
+
+
+
+    
             
     
