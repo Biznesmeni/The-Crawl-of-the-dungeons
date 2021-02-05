eda5267fbf11f56ac19e7e9c425d1bf62b29c383
Dāvis <davis.ositis@gmail.com>
2021-02-05 20:18:31 +0200

    Var ielikt mūziku un skaņas

--- MainMenu/MainMenu.py
+++ MainMenu/MainMenu.py
@@ -1,49 +1,52 @@
 import pygame #importē pygame lai var izmantot funkcijas
+import sys
 
 BackgroundImg = pygame.image.load('Background_Menu(PlaceHolder).png')#atrod attēlu folderī
 BackgroundX = 0 #kur atradīsies attēls x asī
 BackgroundY = 0 #kur atradīsies attēls y asī
 
-GameLogoImg = pygame.image.load('')#Šeit jāieliek spēles logo, vai titles.
+GameLogoImg = pygame.image.load('PlaceHolderLogo.png')#Šeit jāieliek spēles logo, vai titles.
 GamelogoX = 0
 GameLogoY = 0
 
+def PlayMusic():
+    pygame.mixer.music.load('MainMenu.wav') #Šī komanda atrod mūzikas failu
+    pygame.mixer.music.play(-1) #Šī komanda spēlē mūziku bezgalīgi
+
 def background():#izveido jaunu funkciju
-    screen.blit(BackgroundImg, (BackgroundX,BackgroundY)) #izprintē pāsu attēlu
+    screen.blit(BackgroundImg, (BackgroundX,BackgroundY)) #izprintē pasu attēlu
 
 def GameLogo():
-    screen.blit(GameLogoImg, (GamelogoX, GameLogoY))
-    
- 
+    screen.blit(GameLogoImg, (GamelogoX, GameLogoY))    
+
+def PlaySound():
+    hoverSound = pygame.mixer.Sound('')
   
 pygame.init()#vajadzīgs, lai inicializētu funkcijas
 
 screen = pygame.display.set_mode((960,540))#izveido ekrānu (width,heigth)
 
 pygame.display.set_caption('Piemērs')#Pārmaina ekrāna spēles nosaukumu ekrāna kreisajā augšā
+
 icon = pygame.image.load('Sprite-0002.png')#Dabū attēlu ikonai, jābūt 32x32 pikseļi
 pygame.display.set_icon(icon)#Uzliek o Ikonu ekrāna stūrī
 
-#Pārbauda, vai spēle strādā, arī uztver, ja aiztaisa to ciet.
+PlayMusic()#aktivizē definēto darbību
+
 running = True
-while running:
+while running:#Pārbauda, vai spēle strādā, arī uztver, ja aiztaisa to ciet.  
     for event in pygame.event.get():#Pārbauda, vai kaut kas notiek
         if event.type == pygame.QUIT:#uztver klikšķi uz x pogas
             running = False
-    # screen.fill((255,165,0))#aizkrāso ekrānu oranžu RGB - (Red, Green, Blue)
+            pygame.quit()
+            sys.exit()
+    screen.fill((255,165,0))#aizkrāso ekrānu oranžu RGB - (Red, Green, Blue)
     
-    GameLogo()
+    #ieprieks veidotā funkcija
     
-    background()#iepriek veidotā funkcija
+    GameLogo()#ieprieks veidotā funkcija
     
-    pygame.display.update()#Updato ekrānu, lai izdēstu bijusos framus.
+    background()#ieprieks veidotā funkcija
     
-    
-
-
-
-
-
-    
-            
+    pygame.display.update()#Updato ekrānu, lai izdēstu bijusos frames.
     
